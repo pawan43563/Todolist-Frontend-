546190804f33f0823107685a49279797d5eaf46a
pawan sharma <pawan.ps43563@hotmail.com>
2021-08-22 01:00:31 +0530

    Frontend with user login

--- userjs/login.js
+++ userjs/login.js
@@ -27,7 +27,7 @@ export const Login=async (e)=>{
     }
     localStorage.setItem("token",response.token)
     if(response.token){
-        window.location.href="http://127.0.0.1:5500/index.html"
+        window.location.href="http://127.0.0.1:5500/Task.html"
     }
     
         
