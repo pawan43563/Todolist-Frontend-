1c9277fec7dab5ee7ef61e41327d8a21726c70ae
pawan sharma <pawan.ps43563@hotmail.com>
2021-08-25 20:01:43 +0530

    Webpack config

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import { addTask, DisplayAllTasks } from "./actions/domOperations.js";
 import {Toggle} from "./actions/theme.js"
 import {apicall} from "./apicalls/apirequest.js"
-import "../styles/styles.css";
-import "../styles/media-queries.css";
+// import "../styles/styles.css";
+// import "../styles/media-queries.css";
 
 window.onload=()=>{
 
